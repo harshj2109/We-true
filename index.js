45735ca0e61d50dabc00ebba054f8ddbccb05eb3
Harsh Jain <112223901+harshj2109@users.noreply.github.com>
2024-08-22 19:17:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ hamburger.addEventListener('click',()=>{
     navitem.classList.toggle("toggle")
 
 
-    
 })
 
 items.forEach(link=>{
@@ -15,3 +14,38 @@ items.forEach(link=>{
 
     })
 })
+
+
+const serviceOption = document.querySelector(".option-1");
+const productOption = document.querySelector(".option-2");
+const serviceSection = document.querySelector(".sec-2-1");
+const productSection = document.querySelector(".sec-2-2");
+serviceSection.style.display="block"
+
+
+serviceOption.style.backgroundColor = "white";
+serviceOption.style.color="black"
+
+serviceOption.addEventListener("click", function () {
+  
+  serviceSection.style.display="block"
+  productSection.style.display="none"
+  serviceOption.style.backgroundColor = "white";
+serviceOption.style.color="black"
+
+productOption.style.backgroundColor = "black";
+productOption.style.color="white"
+});
+
+productOption.addEventListener("click", () => {
+  serviceSection.style.display="none"
+  productSection.style.display="block"
+
+serviceOption.style.backgroundColor = "black";
+serviceOption.style.color="white"
+
+productOption.style.backgroundColor = "white";
+productOption.style.color="black"
+
+});
+
